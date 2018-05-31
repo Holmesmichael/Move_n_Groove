fb81df75c7f394a62941dfcb3ef918f6d7ad52b4
Michael A. Holmes <holmes.m.andrew@gmail.com>
2018-05-30 22:21:59 -0500

    added Devise

--- app/assets/javascripts/components/greetings.jsx
+++ app/assets/javascripts/components/greetings.jsx
@@ -3,7 +3,7 @@ var Greeting = React.createClass({
   render: function() {
     return (
     <div>
-      <h2>{this.props.activity.activity}</h2>
+      <h2>Hi{this.props.activity.activity}</h2>
     </div>);
   }
 });
