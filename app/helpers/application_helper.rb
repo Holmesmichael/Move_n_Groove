22e778e8d3289cd11f97e14b1fadbe344d29d6a4
Michael A. Holmes <holmes.m.andrew@gmail.com>
2018-06-04 17:18:59 -0500

    basic landing styling, random background images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+        def get_random_image
+          image_path_prefix = "app/assets/images/"
+          image_files = Dir.glob("#{image_path_prefix}backgrounds/*")
+          image_files.sample.split(image_path_prefix)[1]
+        end
+      
 end
